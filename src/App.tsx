6cc280aff9f924fb961ce369b9050982377e07dd
Ivan <ivanlima1993@gmail.com>
2026-02-15 14:34:09 -0300

    ajuste HashRouter e base

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
 import { AddressProvider } from "@/contexts/AddressContext";
 import SplashScreen from "@/components/SplashScreen";
@@ -90,7 +90,7 @@ const App = () => {
             )}
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <HashRouter>
               <GlobalOrderNotifications />
               <Routes>
                 <Route path="/" element={<Index />} />
@@ -146,7 +146,7 @@ const App = () => {
                 <Route path="/sobre-nos" element={<AboutUs />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
-            </BrowserRouter>
+            </HashRouter>
           </TooltipProvider>
         </CartProvider>
       </AddressProvider>
